41692d0209c85542067fc4882506711522f98cb3
Enam AKLI <akli.ena88@gmail.com>
2024-01-15 23:55:11 +0100

    rds_proxy_updated_working_version

--- wordpress_ec2_rds/wordpress_ec2_rds_stack.py
+++ wordpress_ec2_rds/wordpress_ec2_rds_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import Stack
 from constructs import Construct
 from lib.constructs.vpc import CustomVPC
 from lib.config import config
-# from lib.constructs.rds import MySQLRdsInstance
+from lib.constructs.rds import MySQLRdsInstance
 # from lib.constructs.alb import WordpressApplicationLoadBalancer
 # from lib.constructs.ec2 import WordpressAutoScalingGroup
 
@@ -19,19 +19,32 @@ class WordpressEc2RdsStack(Stack):
             'vpc_cidr_block': '10.0.0.0/16',
         })
 
-        # # Define properties for the MySQL RDS instance.
-        # rds_props = {
-        #     'vpc': custom_vpc_instance.vpc,  # VPC where the RDS instance will be deployed.
-        #     'prefix': config['project_name'],  # Prefix for naming the RDS resources.
-        #     'user': 'admin',  # Administrator username for the RDS instance.
-        #     'database': 'wordpress',  # Database name to be created.
-        #     'secret_name': f"{config['project_name']}/rds/mysql/credentials",  # Secret name for the RDS credentials.
-        #     # Additional properties like port, instance size, etc., can be specified here.
-        # }
+        # Define properties for the MySQL RDS instance
+        rds_props = {
+            'vpc': custom_vpc_instance.vpc,
+            'prefix': config['project_name'],
+            'user': 'admin',  # Admin username for the RDS instance
+            'database': 'wordpress',  # Database name
+            'secret_name': '/rds/mysql/credentials'  # Name of the secret in Secrets Manager
+        }
+
+        # Instantiate the MySQL RDS instance
+        mysql_rds_instance = MySQLRdsInstance(self, "MySQLRdsInstance", rds_props)
+        # # This will create an RDS instance and a proxy using the existing secret
+
 
         # # Create the MySQL RDS instance.
         # # This instance will be used by the WordPress application for its database needs.
-        # mysql_rds_instance = MySQLRdsInstance(self, 'MySQLRdsInstance', rds_props)
-        # # The RDS instance is now part of the stack and is integrated within the custom VPC.
+        # Instantiate the WordpressAutoScalingGroup construct
+        # This will create an Auto Scaling Group with EC2 instances configured for WordPress
+        # wordpress_asg = WordpressAutoScalingGroup(
+        #     self,
+        #     "WordpressAutoScalingGroup",
+        #     vpc=custom_vpc_instance.vpc,  # Reference to the custom VPC
+        #     db_secret_name=config['rds']['secret_name'],  # The name of the secret for RDS access
+        #     wp_secret_name=config['wordpress']['secret_name'],  # The name of the secret for WordPress config
+        #     rds_security_group=rds_instance.db_instance.connections.security_groups[0],  # The RDS instance's security group
+        # )
 
-        # Future constructs like EC2 instances, ALB, etc., will be added here in incremental updates.
+        # # Output the Auto Scaling Group's security group ID for reference
+        # cdk.CfnOutput(self, "WordpressASGSecurityGroup", value=wordpress_asg.security_group.security_group_id)
